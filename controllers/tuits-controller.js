6ac75298a10f2c92476013e6aeef0420bbe14845
Roger Kwong <rkwong12@gmail.com>
2022-04-03 17:49:55 -0400

    Make a8 branch

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,29 +1,26 @@
-import {
-  findAllTuitsDao,
-  createTuitDao,
-  deleteTuitDao,
-  updateTuitDao,
-} from "./tuits/tuits-dao.js";
+import posts from "./tuits/tuits.js";
+let tuits = posts;
 
-const createTuit = async (req, res) => {
+const createTuit = (req, res) => {
   const newTuit = req.body;
-  const insertedTuit = await createTuitDao(newTuit);
-  res.json(insertedTuit);
+  newTuit._id = new Date().getTime() + "";
+  newTuit.likes = 0;
+  tuits.push(newTuit);
+  res.json(newTuit);
 };
-const findAllTuits = async (req, res) => {
-  const tuits = await findAllTuitsDao();
+const findAllTuits = (req, res) => {
   res.json(tuits);
 };
-const updateTuit = async (req, res) => {
+const updateTuit = (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updatedTuit = req.body;
-  const status = await updateTuitDao(tuitdIdToUpdate, updatedTuit);
-  res.sendStatus(status);
+  tuits = tuits.map((t) => (t._id === tuitdIdToUpdate ? updatedTuit : t));
+  res.sendStatus(200);
 };
-const deleteTuit = async (req, res) => {
+const deleteTuit = (req, res) => {
   const tuitdIdToDelete = req.params.tid;
-  const status = await deleteTuitDao(tuitdIdToDelete);
-  res.sendStatus(status);
+  tuits = tuits.filter((t) => t._id !== tuitdIdToDelete);
+  res.sendStatus(200);
 };
 
 export default (app) => {
