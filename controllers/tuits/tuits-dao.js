1946eddbee0ff94c7de402e18615545770421fd0
Roger Kwong <rkwong12@gmail.com>
2022-04-19 21:42:21 -0400

    Fix issue with status codes

--- controllers/tuits/tuits-dao.js
+++ controllers/tuits/tuits-dao.js
@@ -2,6 +2,11 @@ import tuitsModel from "./tuits-model.js";
 
 export const findAllTuitsDao = () => tuitsModel.find();
 export const createTuitDao = (tuit) => tuitsModel.create(tuit);
-export const deleteTuitDao = (tid) => tuitsModel.deleteOne({ _id: tid });
-export const updateTuitDao = (tid, tuit) =>
+export const deleteTuitDao = (tid) => {
+  tuitsModel.deleteOne({ _id: tid });
+  return 200;
+};
+export const updateTuitDao = (tid, tuit) => {
   tuitsModel.updateOne({ _id: tid }, { $set: tuit });
+  return 200;
+};
